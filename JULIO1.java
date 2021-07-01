f8e7b4a32026afbd956853d16f2e8278a98da2e1
MIGUEL31-00 <tu_correo_de_GITHUB>
2021-07-01 18:48:15 -0500

    QUITE UN ALINEA

--- JULIO1.java
+++ JULIO1.java
@@ -2,6 +2,5 @@ public class JULIO1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("\u00F6");
-        System.out.println("SUPER HACKER");
     }
 }
